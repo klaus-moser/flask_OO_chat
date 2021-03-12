52d0458d9a7a78b65c7b117a307dd267bb14f943
Don Kanaille <60796711+don-kanaille@users.noreply.github.com>
2021-03-12 22:01:25 +0100

    Add .html files

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
-import socketio
-
-sio = socketio.Server()
-app = socketio.WSGIApp(sio, static_files={
-    '/': './public/'
-})
+from flask import Flask, render_template
 
 
-@sio.event
-def connect(sid: str, environ: dict) -> None:
-    """
-    Connect event from client to server.
-    :param sid: Session id created by socket server.
-    :param environ: Headers, cookies etc. from client.
-    :return: None.
-    """
-    print(sid, 'connected')
+app = Flask(__name__)
+
+app.secret_key = 'replace key later'
+url = 'postgres://atybdepvdbtiss:eff908bf955faa4f2363bfca5f52db6fddc399aa15cc609510332bdbcdb6eeaf@ec2-54-220-35-19.eu-west-1.compute.amazonaws.com:5432/d1lqgh91p1te6o'
 
 
-@sio.event
-def disconnect(sid: str) -> None:
-    """
-    Disconnect server from client.
-    :param sid: Session id.
-    :return: None.
-    """
-    print(sid, 'disconnected')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1', port=5000)
