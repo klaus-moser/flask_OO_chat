95bcb49b672acddfd90ead8ef3e8686f36c86161
Don Kanaille <60796711+don-kanaille@users.noreply.github.com>
2021-03-12 23:09:50 +0100

    Add form_helper.html

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 
 from wtform_fields import RegistrationForm
 
+
 app = Flask(__name__)
 
+# Configs
 app.secret_key = 'replace key later'
-url = 'postgres://atybdepvdbtiss:eff908bf955faa4f2363bfca5f52db6fddc399aa15cc609510332bdbcdb6eeaf@ec2-54-220-35-19.eu-west-1.compute.amazonaws.com:5432/d1lqgh91p1te6o'
 
 
 @app.route('/', methods=['GET', 'POST'])
