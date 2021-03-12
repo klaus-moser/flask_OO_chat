81ef4d0be6f3fbff1f94ffb5db09eb5755111204
Don Kanaille <60796711+don-kanaille@users.noreply.github.com>
2021-03-12 16:19:49 +0100

    Init Files

---
+++ app.py
@@ -0,0 +1,25 @@
+import socketio
+
+sio = socketio.Server()
+app = socketio.WSGIApp(sio)
+
+
+@sio.event
+def connect(sid: str, environ: dict) -> None:
+    """
+    Connect event from client to server.
+    :param sid: Session id created by socket server.
+    :param environ: Headers, cookies etc. from client.
+    :return: None.
+    """
+    print(sid, 'connected')
+
+
+@sio.event
+def disconnect(sid: str) -> None:
+    """
+    Disconnect server from client.
+    :param sid: Session id.
+    :return: None.
+    """
+    print(sid, 'disconnected')
