26b3ff535bbce503f56486576c2cfbcf687a4696
Don Kanaille <60796711+don-kanaille@users.noreply.github.com>
2021-03-14 21:42:52 +0100

    Login via @login_required

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 from passlib.hash import pbkdf2_sha256
 from os import environ
 
 from src.wtform_fields import RegistrationForm, LoginForm
 from models.user import UserModel
 from src.db import db
-
 
 # Configure app
 app = Flask(__name__)
@@ -15,6 +15,15 @@ app.secret_key = ']K~B;aF>5/`/]h3xoZ8'
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+
+# Configure flask login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return UserModel.find_by_id(id_=user_id)
 
 
 # Create database before first request
@@ -61,10 +70,35 @@ def login():
 
     # Allow login if validation succeeded [POST]
     if login_form.validate_on_submit():
-        return "Logged in!"
+
+        # Login user
+        user = UserModel.find_by_username(username=login_form.username.data)
+        login_user(user=user)
+
+        # Check if user is logged in
+        if current_user.is_authenticated:
+            return "Logged in!"
+        return "NOT logged in ..."
 
     # Return login page [GET]
     return render_template("login.html", form=login_form)
+
+
+@app.route('/chat', methods=['GET', 'POST'])
+@login_required  # flask-login: Decorator to check if/that user is logged in
+def chat():
+
+
+    return "Lets chat!"
+
+
+@app.route('/logout', methods=['GET'])
+@login_required  # flask-login: Decorator to check if/that user is logged in
+def logout():
+
+    # Logout user
+    logout_user()
+    return "Logged out!"
 
 
 if __name__ == "__main__":
